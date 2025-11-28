1384501c3b8253ebe950fad016a923b79b6f91ef
Sarayasserrr <es.sara.yasser2023@alexu.edu.eg>
2025-11-28 22:56:52 +0200

    Mode 3 insha'allah

--- src/Mode3Validator.java
+++ src/Mode3Validator.java
@@ -1,8 +1,39 @@
 public class Mode3Validator implements Validator {
+
     @Override
     public ValidationResult validate(SudokuBoard board) {
+
+        // create the 3 checkers
+        RowChecker rowChecker = new RowChecker(board);
+        ColumnChecker colChecker = new ColumnChecker(board);
+        BoxChecker boxChecker = new BoxChecker(board);
+
+        // each checker inside a thread
+        Thread t1 = new Thread(rowChecker);
+        Thread t2 = new Thread(colChecker);
+        Thread t3 = new Thread(boxChecker);
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        // wait for all threads to finish
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // collect results into ValidationResult
         ValidationResult result = new ValidationResult();
-//complete rest of code
+        result.addRowErrors(rowChecker.getResult());
+        result.addColumnErrors(colChecker.getResult());
+        result.addBoxErrors(boxChecker.getResult());
+
+        // return final validation result
         return result;
     }
 }
+
